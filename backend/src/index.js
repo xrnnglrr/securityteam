f2fcd0e6a70bc13c6e7f69a326a79fa6ec487c06
xrnnglrr <xernan.galora0@gmail.com>
2026-07-29 22:03:01 +0800

    Add user and message models, implement MongoDB connection, and enhance server setup

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,8 +1,27 @@
 //const express = require("express");
 import express from "express";
+import cors from "cors";
+
 import "dotenv/config"
+
+import { clerkMiddleware } from "@clerk/express";
+
+import User from "./models/user.model.js";
+import { connectDB } from "./lib/db.js";
 
 const app = express();
 const PORT = process.env.PORT
+const FRONTEND_URL = process.env.FRONTEND_URL;
 
-app.listen(PORT, () => console.log("Server is up and running on port 3000"));
+app.use(express.json());
+app.use (cors({origin:FRONTEND_URL, credentials:true}));
+app.use(clerkMiddleware());
+
+app.get("/health", (req, res) => {
+    res.status(200).json({ ok:true});
+});
+
+app.listen(PORT, () => {
+    connectDB();
+    console.log("Server is up and running on port 3000")
+});
---
+++ backend/src/lib/db.js
@@ -0,0 +1,20 @@
+import mongoose from "mongoose";
+
+export async function connectDB() {
+    try {
+        const mongoUri = process.env.MONGO_URI;
+
+        if (!mongoUri) {
+            throw new Error("MONGO_URI is required");
+            
+        }
+
+        const conn = await mongoose.connect(mongoUri);
+
+        console.log("MongoDB connected", conn.connection.host);
+    } catch (error) {
+        console.error("MongoDB connection error:", error.message);
+        process.exit(1);
+        //means failed
+    }    
+}
