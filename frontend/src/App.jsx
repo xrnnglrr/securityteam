be9a2a7d2562708e89765ac1d1e33af2d5c98f3d
xrnnglrr <xernan.galora0@gmail.com>
2026-08-05 00:40:28 +0800

    feat: add authentication components and loading states, implement theme and wallpaper pickers

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -5,19 +5,20 @@ import { Navigate, Route, Routes } from 'react-router';
 import ChatPage from './pages/ChatPage';
 import AuthPage from './pages/AuthPage';
 import { useAuth } from '@clerk/react';
+import PageLoader from './components/PageLoader';
 
 function App() {
   
   const { isSignedIn, isLoaded } = useAuth();
 
-  if (!isLoaded) return <p>loading...</p>;
+  if (!isLoaded) return <PageLoader />
 
   return (
     <ThemeProvider>
       <WallpaperProvider>
         <Routes>
           <Route path="/" element={isSignedIn ? <ChatPage /> : <Navigate to={"/auth"} replace />} />
-          <Route path="/auth" element={!isSignedIn ? <AuthPage /> : <Navigate to={"/chat"} replace />} />
+          <Route path="/auth" element={!isSignedIn ? <AuthPage /> : <Navigate to={"/"} replace />} />
 
         </Routes>
       </WallpaperProvider>
