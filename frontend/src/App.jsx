4a8c6c3e16480b376e9f94d970e97099b53d150a
xrnnglrr <xernan.galora0@gmail.com>
2026-08-04 23:47:19 +0800

    feat: implement theme and wallpaper context providers
    - Added ThemeContext and WallpaperContext to manage application themes and wallpapers.
    - Created herouiThemePresets.js and wallpapers.js to store theme presets and wallpaper data.
    - Introduced ThemeProvider and WallpaperProvider components for context management.
    - Implemented theme switching functionality with local storage support.
    - Added new AuthPage and ChatPage components for future routing.
    - Updated main.jsx to include BrowserRouter for routing capabilities.
    - Removed unused vite.svg asset and cleaned up index.css for Tailwind integration.

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,24 +1,28 @@
-import './App.css'
-import { Show, SignInButton, SignUpButton, UserButton } from '@clerk/react'
+
+import { WallpaperProvider } from './context/WallpaperContext';
+import { ThemeProvider } from './context/ThemeContext';
+import { Navigate, Route, Routes } from 'react-router';
+import ChatPage from './pages/ChatPage';
+import AuthPage from './pages/AuthPage';
+import { useAuth } from '@clerk/react';
 
 function App() {
   
+  const { isSignedIn, isLoaded } = useAuth();
+
+  if (!isLoaded) return <p>loading...</p>;
 
   return (
-    <div>
-      <h1>Log In</h1>
+    <ThemeProvider>
+      <WallpaperProvider>
+        <Routes>
+          <Route path="/" element={isSignedIn ? <ChatPage /> : <Navigate to={"/auth"} replace />} />
+          <Route path="/auth" element={!isSignedIn ? <AuthPage /> : <Navigate to={"/chat"} replace />} />
 
-      <header>
-        <Show when="signed-out">
-          <SignInButton mode="modal" />
-          <SignUpButton mode="modal"  />
-        </Show>
-        <Show when="signed-in">
-          <UserButton />
-        </Show>
-      </header>
-    </div>
+        </Routes>
+      </WallpaperProvider>
+    </ThemeProvider>
   )
 }
 
-export default App
+export default App;
--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,12 +3,16 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import { ClerkProvider } from '@clerk/react'
+import {BrowserRouter} from "react-router";
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <ClerkProvider>
-      <App />
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
     </ClerkProvider>
+    
     
   </StrictMode>,
 )
